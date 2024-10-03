b4ba396c353c88f28afa055f72e2bbbde277a36a
yangtaewoong <tom0323@naver.com>
2024-10-03 18:00:02 +0900

    화살표 추적 완료

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -8,27 +8,34 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 arrow = load_image('hand_arrow.png')
 
-def handle_events():
-    global running
-    global x, y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-
 def arrow_events():
     global ax,ay
     ax = randint(10,TUK_WIDTH - 50 )
     ay = randint(10, TUK_HEIGHT - 50)
 
+def move_character():
+    global x,y,arrow_x, arrow_y
+    speed = 0.1
+    if x < arrow_x:
+        x += min(speed, arrow_x - x)
+    elif x > arrow_x:
+        x -= min(speed, x - arrow_x)
+
+    if y < arrow_y:
+        y += min(speed, arrow_y - y)
+    elif y > arrow_y:
+        y -= min(speed, y - arrow_y)
+
+    if x == arrow_x and y == arrow_y:
+        delay(0.5)
+        arrow_events()
+        arrow_x, arrow_y = ax, ay
+
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+arrow_x, arrow_y = x, y
 frame = 0
 ax, ay =0, 0
 arrow_events()
@@ -37,12 +44,14 @@ hide_cursor()
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    if x<= arrow_x:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    elif x> arrow_x:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
     arrow.draw(ax, ay)
     update_canvas()
     frame = (frame + 1) % 8
-
-    handle_events()
+    move_character()
 
 close_canvas()
 
